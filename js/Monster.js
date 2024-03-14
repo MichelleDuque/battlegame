afd9f442204b7ffa22b7a4af4271cc61c47ef86c
MichelleDuque <michelled_1994@hotmail.com>
2024-03-14 13:55:55 -0400

    added modules

---
+++ js/Monster.js
@@ -0,0 +1,14 @@
+class Monster {
+    constructor(
+        name, 
+        image,
+        life
+        ) {
+        this.name = name;
+        this.image = image;
+        this.life = life;
+        this.attacks = [];
+    }
+}
+
+export {Monster}
